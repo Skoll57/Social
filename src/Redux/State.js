90805b8f36401d7b1d374adcbb1936175782d43e
Skoll <tabolskysergei@yandex.ru>
2023-07-02 11:17:53 +0300

    store and bind()

--- src/Redux/State.js
+++ src/Redux/State.js
@@ -1,170 +1,176 @@
-let rerenderEntireTree;
-
-let state = {
-  mainPage: {
-    posts: [
-      {
-        message: "Hi! What are you doing?",
-        likeCount: 13,
-        dislikeCount: 3,
-        id: 1,
-      },
-      {
-        message: "Hello! I'am testing props.",
-        likeCount: 23,
-        dislikeCount: 5,
-        id: 2,
-      },
-      {
-        message: "It turns out?",
-        likeCount: 8,
-        dislikeCount: 1,
-        id: 3,
-      },
-      {
-        message:
-          "I'm moving towards my goal with small steps... And i need more word! More! more! more! I'm moving towards my goal with small steps... And i need more word! More! more! more!",
-        likeCount: 14,
-        dislikeCount: 2,
-        id: 4,
-      },
-      {
-        message: "I'm happy for you!",
-        likeCount: 1,
-        dislikeCount: 0,
-        id: 5,
-      },
-    ],
-    newPostText: "",
+let store = {
+  _state: {
+    mainPage: {
+      posts: [
+        {
+          message: "Hi! What are you doing?",
+          likeCount: 13,
+          dislikeCount: 3,
+          id: 1,
+        },
+        {
+          message: "Hello! I'am testing props.",
+          likeCount: 23,
+          dislikeCount: 5,
+          id: 2,
+        },
+        {
+          message: "It turns out?",
+          likeCount: 8,
+          dislikeCount: 1,
+          id: 3,
+        },
+        {
+          message:
+            "I'm moving towards my goal with small steps... And i need more word! More! more! more! I'm moving towards my goal with small steps... And i need more word! More! more! more!",
+          likeCount: 14,
+          dislikeCount: 2,
+          id: 4,
+        },
+        {
+          message: "I'm happy for you!",
+          likeCount: 1,
+          dislikeCount: 0,
+          id: 5,
+        },
+      ],
+      newPostText: "",
+    },
+    dialogsPage: {
+      persone: [
+        {
+          id: 1,
+          fullName: "Skoll Sokolov",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXZjLyV_KDdDu7o-wlFTQD3kf6E2cqIvQRfFvbDBIRWokc_wK_5pkeNxgHk8_07xfn9Sg&usqp=CAU",
+        },
+        {
+          id: 2,
+          fullName: "Ivan Krest",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__5kW0KKHFHa5A8ulgazLinxGvQeYQRNBow&usqp=CAU",
+        },
+        {
+          id: 3,
+          fullName: "Julia Sexy",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvPOx14pvsWJv5D3eYtZkbKCW1eGBoVewhoA&usqp=CAU",
+        },
+        {
+          id: 4,
+          fullName: "Vovka Barchuk",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UB9-_daPV0T1Sw2E7kfEJsyp2VDVFsUWmQ&usqp=CAU",
+        },
+        {
+          id: 5,
+          fullName: "Nikita Zadira",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjhjC_CIPIkVcqkhvFkvTpRvnaxxfhAN2uMg&usqp=CAU",
+        },
+        {
+          id: 6,
+          fullName: "Elen Prekrasnaya",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFtTwXSsU-Pju-DU8LKAf1_422YMvDKCdg6tQ5GyaxmqdPb4F7mU6w7RX4JYai9PyAW2s&usqp=CAU",
+        },
+      ],
+      messages: [
+        { id: 1, message: "HI" },
+        { id: 2, message: "HELLO" },
+        { id: 3, message: "HEY" },
+      ],
+      newMessageText: "",
+    },
+    friendsPage: {
+      persone: [
+        {
+          id: 1,
+          fullName: "Skoll Sokolov",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXZjLyV_KDdDu7o-wlFTQD3kf6E2cqIvQRfFvbDBIRWokc_wK_5pkeNxgHk8_07xfn9Sg&usqp=CAU",
+        },
+        {
+          id: 2,
+          fullName: "Ivan Krest",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__5kW0KKHFHa5A8ulgazLinxGvQeYQRNBow&usqp=CAU",
+        },
+        {
+          id: 3,
+          fullName: "Julia Sexy",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvPOx14pvsWJv5D3eYtZkbKCW1eGBoVewhoA&usqp=CAU",
+        },
+        {
+          id: 4,
+          fullName: "Vovka Barchuk",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UB9-_daPV0T1Sw2E7kfEJsyp2VDVFsUWmQ&usqp=CAU",
+        },
+        {
+          id: 5,
+          fullName: "Nikita Zadira",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjhjC_CIPIkVcqkhvFkvTpRvnaxxfhAN2uMg&usqp=CAU",
+        },
+        {
+          id: 6,
+          fullName: "Elen Prekrasnaya",
+          avatar:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFtTwXSsU-Pju-DU8LKAf1_422YMvDKCdg6tQ5GyaxmqdPb4F7mU6w7RX4JYai9PyAW2s&usqp=CAU",
+        },
+      ],
+    },
   },
-  dialogsPage: {
-    persone: [
-      {
-        id: 1,
-        fullName: "Skoll Sokolov",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXZjLyV_KDdDu7o-wlFTQD3kf6E2cqIvQRfFvbDBIRWokc_wK_5pkeNxgHk8_07xfn9Sg&usqp=CAU",
-      },
-      {
-        id: 2,
-        fullName: "Ivan Krest",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__5kW0KKHFHa5A8ulgazLinxGvQeYQRNBow&usqp=CAU",
-      },
-      {
-        id: 3,
-        fullName: "Julia Sexy",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvPOx14pvsWJv5D3eYtZkbKCW1eGBoVewhoA&usqp=CAU",
-      },
-      {
-        id: 4,
-        fullName: "Vovka Barchuk",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UB9-_daPV0T1Sw2E7kfEJsyp2VDVFsUWmQ&usqp=CAU",
-      },
-      {
-        id: 5,
-        fullName: "Nikita Zadira",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjhjC_CIPIkVcqkhvFkvTpRvnaxxfhAN2uMg&usqp=CAU",
-      },
-      {
-        id: 6,
-        fullName: "Elen Prekrasnaya",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFtTwXSsU-Pju-DU8LKAf1_422YMvDKCdg6tQ5GyaxmqdPb4F7mU6w7RX4JYai9PyAW2s&usqp=CAU",
-      },
-    ],
-    messages: [
-      { id: 1, message: "HI" },
-      { id: 2, message: "HELLO" },
-      { id: 3, message: "HEY" },
-    ],
-    newMessageText: "",
+
+  getState() {
+    return this._state;
   },
-  friendsPage: {
-    persone: [
-      {
-        id: 1,
-        fullName: "Skoll Sokolov",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXZjLyV_KDdDu7o-wlFTQD3kf6E2cqIvQRfFvbDBIRWokc_wK_5pkeNxgHk8_07xfn9Sg&usqp=CAU",
-      },
-      {
-        id: 2,
-        fullName: "Ivan Krest",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR__5kW0KKHFHa5A8ulgazLinxGvQeYQRNBow&usqp=CAU",
-      },
-      {
-        id: 3,
-        fullName: "Julia Sexy",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvPOx14pvsWJv5D3eYtZkbKCW1eGBoVewhoA&usqp=CAU",
-      },
-      {
-        id: 4,
-        fullName: "Vovka Barchuk",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6UB9-_daPV0T1Sw2E7kfEJsyp2VDVFsUWmQ&usqp=CAU",
-      },
-      {
-        id: 5,
-        fullName: "Nikita Zadira",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjhjC_CIPIkVcqkhvFkvTpRvnaxxfhAN2uMg&usqp=CAU",
-      },
-      {
-        id: 6,
-        fullName: "Elen Prekrasnaya",
-        avatar:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFtTwXSsU-Pju-DU8LKAf1_422YMvDKCdg6tQ5GyaxmqdPb4F7mU6w7RX4JYai9PyAW2s&usqp=CAU",
-      },
-    ],
+
+  _callSubscriber() {},
+
+  addPost() {
+    let newPost = {
+      id: 6,
+      message: this._state.mainPage.newPostText,
+      likeCount: 0,
+      dislikeCount: 0,
+    };
+
+    if (newPost.message.length >= 1 && newPost.message !== " ") {
+      this._state.mainPage.posts.push(newPost);
+      this._state.mainPage.newPostText = "";
+      this._callSubscriber(this._state);
+    }
+  },
+
+  addMessage() {
+    let newMessage = {
+      id: 4,
+      message: this._state.dialogsPage.newMessageText,
+    };
+
+    if (newMessage.message.length >= 1 && newMessage.message !== " ") {
+      this._state.dialogsPage.messages.push(newMessage);
+      this._state.dialogsPage.newMessageText = "";
+      this._callSubscriber(this._state);
+    }
+  },
+
+  updateMessageText(messageText) {
+    this._state.dialogsPage.newMessageText = messageText;
+    this._callSubscriber(this._state);
+  },
+
+  updateNewPostText(postMessage) {
+    this._state.mainPage.newPostText = postMessage;
+    this._callSubscriber(this._state);
+  },
+
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-export let addPost = () => {
-  let newPost = {
-    id: 6,
-    message: state.mainPage.newPostText,
-    likeCount: 0,
-    dislikeCount: 0,
-  };
-
-  if (newPost.message.length >= 1 && newPost.message !== " ") {
-    state.mainPage.posts.push(newPost);
-    state.mainPage.newPostText = "";
-    rerenderEntireTree(state);
-  }
-};
-
-export let addMessage = () => {
-  let newMessage = {
-    id: 4,
-    message: state.dialogsPage.newMessageText,
-  };
-
-  if (newMessage.message.length >= 1 && newMessage.message !== " ") {
-    state.dialogsPage.messages.push(newMessage);
-    state.dialogsPage.newMessageText = "";
-    rerenderEntireTree(state);
-  }
-};
-
-export let updatePostText = (postMessage) => {
-  state.mainPage.newPostText = postMessage;
-  rerenderEntireTree(state);
-};
-
-export let updateMessageText = (messageText) => {
-  state.dialogsPage.newMessageText = messageText;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import state from "./Redux/State";
+import store from "./Redux/State";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {
-  addPost,
-  addMessage,
-  updatePostText,
-  updateMessageText,
-  subscribe,
-} from "./Redux/State";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -19,15 +12,15 @@ let rerenderEntireTree = (state) => {
     <BrowserRouter>
       <App
         state={state}
-        addPost={addPost}
-        addMessage={addMessage}
-        updateNewPostText={updatePostText}
-        updateMessageText={updateMessageText}
+        addPost={store.addPost.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        updateMessageText={store.updateMessageText.bind(store)}
       />
     </BrowserRouter>
   );
 };
 
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
 
-rerenderEntireTree(state);
+store.subscribe(rerenderEntireTree);
